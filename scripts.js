d0be949c5c84b4b72cd54c06b2b74b9a5cd861a0
Eren Saglam <eren.saglam@berkeley.edu>
2024-04-09 17:19:13 -0700

    done :)

--- scripts.js
+++ scripts.js
@@ -1,94 +1,131 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
-
-
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
+// Default movies
+const defaultCatalog = [
+    {
+        title: "Fresh Prince of Bel Air",
+        posterURL: "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg",
+        description: "A street-smart teenager from Philadelphia is sent to live with his wealthy relatives in a Bel Air mansion.",
+        genre: "Comedy"
+    },
+    {
+        title: "Curb Your Enthusiasm",
+        posterURL: "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg",
+        description: "The off-kilter, unscripted comic vision of Larry David, who plays himself in a parallel universe.",
+        genre: "Comedy"
+    },
+    {
+        title: "East Los High",
+        posterURL: "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg",
+        description: "A group of friends at East Los High face the trials and tribulations of teenagers growing up in East Los Angeles.",
+        genre: "Drama"
+    }
 ];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
+ 
+function loadCatalog() {
+    let localStorageCatalog = JSON.parse(localStorage.getItem('catalog'));
+    // If local catalog is empty => add missing default movies
+    if (!localStorageCatalog) {
+        localStorageCatalog = defaultCatalog.slice();
+    } else {
+        const missingMovies = defaultCatalog.filter(movie => !localStorageCatalog.some(localMovie => localMovie.title === movie.title));
+        localStorageCatalog.push(...missingMovies);
+    }
 
+    return localStorageCatalog;
+}
 
-// This function adds cards the page to display the data in the array
-function showCards() {
+let catalog = loadCatalog();
+
+function saveCatalog() {
+    localStorage.setItem('catalog', JSON.stringify(catalog));
+}
+function editMovie(index) {
+    const movie = catalog[index];
+    // Prompt user for new details
+    const newTitle = prompt("Enter the new title of the TV show:", movie.title);
+    const newPosterURL = prompt("Enter the new URL of the poster image:", movie.posterURL);
+    const newDescription = prompt("Enter the new description of the TV show:", movie.description);
+    const newGenre = prompt("Enter the new genre of the TV show:", movie.genre);
+    movie.title = newTitle;
+    movie.posterURL = newPosterURL;
+    movie.description = newDescription;
+    movie.genre = newGenre;
+    saveCatalog();
+    showCards(catalog);
+}
+
+function showCards(shows) {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
     const templateCard = document.querySelector(".card");
-    
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
-    }
+    shows.forEach((show, index) => {
+        const nextCard = templateCard.cloneNode(true);
+        editCardContent(nextCard, show.title, show.posterURL, show.description, show.genre);
+        nextCard.addEventListener("click", () => editMovie(index)); // Attach click event listener
+        cardContainer.appendChild(nextCard);
+    });
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+
+function editCardContent(card, title, imageURL, description, genre) {
     card.style.display = "block";
-
     const cardHeader = card.querySelector("h2");
-    cardHeader.textContent = newTitle;
-
+    cardHeader.textContent = title;
     const cardImage = card.querySelector("img");
-    cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
-
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
+    cardImage.src = imageURL;
+    cardImage.alt = title + " Poster";
+    const cardDescription = card.querySelector("p.description");
+    cardDescription.textContent = description;
+    const cardGenre = card.querySelector("p.genre");
+    cardGenre.textContent = "Genre: " + genre;
 }
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
+// for showing cards on page load
+document.addEventListener("DOMContentLoaded", () => {
+    showCards(catalog);
+});
 
-function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
+function addShow() {
+    const title = prompt("Enter the title of the TV show:");
+    const posterURL = prompt("Enter the URL of the poster image:");
+    const description = prompt("Enter the description of the TV show:");
+    const genre = prompt("Enter the genre of the TV show:");
+    const newShow = {
+        title: title,
+        posterURL: posterURL,
+        description: description,
+        genre: genre
+    };
+    catalog.push(newShow);
+    saveCatalog();
+    showCards(catalog);
 }
 
 function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
+    catalog.pop();
+    saveCatalog();
+    showCards(catalog);
+}
+
+function searchShows() {
+    const searchTerm = prompt("Enter the title to search for:");
+    const searchResult = catalog.filter(show => show.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    if (searchResult.length > 0) {
+        showCards(searchResult);
+    } else {
+        alert("No results found for '" + searchTerm + "'");
+    }
+}
+
+function filterByGenre() {
+    const selectedGenre = prompt("Enter the genre to filter by:");
+    const filteredCatalog = catalog.filter(show => show.genre.toLowerCase() === selectedGenre.toLowerCase());
+    if (filteredCatalog.length > 0) {
+        showCards(filteredCatalog);
+    } else {
+        alert("No shows found for genre: " + selectedGenre);
+    }
+}
+
+function showAll() {
+    showCards(catalog);
 }
